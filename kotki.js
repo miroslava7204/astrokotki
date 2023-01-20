a2baf1acebee0318b3445835c119f1f35f159585
Miroslava Lazarova <miroslava.m.lazarova@gmail.com>
2023-01-20 13:42:29 +0200

    convertor + grid

--- kotki.js
+++ kotki.js
@@ -1,36 +1,13 @@
-let canvas = document.getElementById("myCanvas");
-let context = canvas.getContext("2d");
-
-let sqx = 50;
-let sqy = 50;
-let img = new Image();
-img.src = " ";
-
-function drawEmptyGrid() {
-    for(let i = 0; i < canvas.height / sqy; i++) {
-        for(let j = 0; j < canvas.width / sqx; j++) {
-            context.fillStyle = "white";
-            context.fillRect(sqx * j, sqy * i, sqx, sqy);
-            context.strokeRect(sqx * j, sqy * i, sqx, sqy);
-        }
-    }
+//целзий -> фаренхайт
+document.getElementById("buttonA").onclick = function () {
+	let inputA = document.getElementById("inputA").value;
+    let a = Number(inputA);
+    document.getElementById("answerA").innerHTML = (a*1.8)+32;
 }
 
-drawEmptyGrid();
-
-canvas.onclick = function(e) {
-    let x = e.clientX;
-    let y = e.clientY;
-    herosqx = Math.floor(x / sqx);
-    herosqy = Math.floor(y / sqy);
-    drawEmptyGrid();
-    context.fillStyle = "black";
-    context.drawImage(img, sqx * herosqx, sqy * herosqy , sqx, sqy);
+//километри -> мили
+document.getElementById("buttonB").onclick = function () {
+	let inputA = document.getElementById("inputB").value;
+    let b = Number(inputA);
+    document.getElementById("answerB").innerHTML = Math.round(b*0.6214);
 }
-
-/*
-context.fillStyle = "green";
-context.strokeStyle = "orange";
-context.fillRect(10, 20, 10, 70);
-context.strokeRect(50, 30, 70, 100);
-*/
